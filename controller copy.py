698458ec0ba93c5c894b55f3be4dc44b9a4ed39c
pvpet <pvpetrus@wp.pl>
2023-10-17 14:50:40 +0200

    Default paint

---
+++ controller copy.py
@@ -0,0 +1,80 @@
+from model import *
+from view import init_grid, get_row_col_from_pos, draw
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Paint +")
+
+
+running = True
+clock = pygame.time.Clock()
+grid = init_grid(ROWS, COLS, BG_COLOR)
+drawing_color = BLACK
+current_drawing_mode = "DOT"
+start_pos_x = None
+start_pos_y = None
+end_pos_x = None
+end_pos_y = None
+
+
+button_y = HEIGHT - TOOLBAR_HEIGHT/2 - 25
+buttons = [
+    Button(10, button_y, 100, 50, BLACK, "LINE", WHITE),
+    Button(120, button_y, 100, 50, BLACK, "DOT", WHITE),
+    Button(230, button_y, 100, 50, BLACK, "TEXT", WHITE),
+]
+
+while running:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if pygame.mouse.get_pressed()[0]:
+            pos = pygame.mouse.get_pos()
+            try:
+                # Clicking on drawing space
+                # Check drawing mode and choose actions accordingly
+                if current_drawing_mode == "DOT":
+                    start_pos_x = None
+                    start_pos_y = None
+                    end_pos_x = None
+                    end_pos_y = None
+                    row, col = get_row_col_from_pos(pos)
+                    grid[row][col] = drawing_color
+                if current_drawing_mode == "LINE":
+                    row, col = get_row_col_from_pos(pos)
+                    if start_pos_x is not None:
+                        end_pos_x = col
+                        end_pos_y = row
+                        pygame.draw.line(WIN, BLUE, (start_pos_x * PIXEL_SIZE, start_pos_y * PIXEL_SIZE),
+                                     (end_pos_x * PIXEL_SIZE, end_pos_y * PIXEL_SIZE), 5)
+                        pygame.display.flip()
+                        pygame.display.update()
+                    else:
+                        start_pos_x = col
+                        start_pos_y = row
+                if current_drawing_mode == "TEXT":
+                    start_pos_x = None
+                    start_pos_y = None
+                    end_pos_x = None
+                    end_pos_y = None
+                    row, col = get_row_col_from_pos(pos)
+            except IndexError:
+                # Clicking on toolbar space
+                for button in buttons:
+                    if not button.clicked(pos):
+                        button.color = BLACK
+                        continue
+                    button.color = GREEN
+                    # Choosing current operation type
+                    if button.text == "DOT":
+                        current_drawing_mode = "DOT"
+                    if button.text == "LINE":
+                        current_drawing_mode = "LINE"
+                    if button.text == "TEXT":
+                        current_drawing_mode = "TEXT"
+    draw(WIN, grid, buttons)
+
+rect = pygame.Rect(0, 0, 600, 600)
+sub = WIN.subsurface(rect)
+pygame.image.save(sub, "screenshot.jpeg")
+pygame.quit()
---
+++ view.py
@@ -0,0 +1,37 @@
+from model import *
+
+
+def init_grid(rows, cols, color):
+    return [[color for _ in range(cols)] for _ in range(rows)]
+
+
+def draw(win, grid, buttons):
+    win.fill(BG_COLOR)
+    draw_grid(win, grid)
+    for button in buttons:
+        button.draw(win)
+    pygame.display.update()
+
+
+def draw_grid(win, grid):
+    for i, row in enumerate(grid):
+        for j, pixel in enumerate(row):
+            pygame.draw.rect(win, pixel, (j * PIXEL_SIZE, i * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE))
+
+    if DRAW_GRID_LINES:
+        for i in range(ROWS + 1):
+            pygame.draw.line(win, BLACK, (0, i * PIXEL_SIZE), (WIDTH, i * PIXEL_SIZE))
+
+        for i in range(COLS + 1):
+            pygame.draw.line(win, BLACK, (i * PIXEL_SIZE, 0), (i * PIXEL_SIZE, HEIGHT - TOOLBAR_HEIGHT))
+
+
+def get_row_col_from_pos(pos):
+    x, y = pos
+    row = y // PIXEL_SIZE
+    col = x // PIXEL_SIZE
+
+    if row >= ROWS:
+        raise IndexError
+
+    return row, col
